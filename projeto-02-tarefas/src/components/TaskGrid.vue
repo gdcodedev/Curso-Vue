0d631362bb1c1d5054c9630a9a90bf8061540d1a
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-05-21 15:12:30 -0300

    Base

---
+++ projeto-02-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+        </template>
+        <p v-else class="no-task">Sem tarefas a fazer</p>
+    </div>
+  
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+    components: { Task },
+    props: {
+        tasks: { type: Array, required: true}
+    }
+
+}
+</script>
+
+
+<style>
+
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+
+    }
+     
+      .no-task {
+          color:  #AAA;
+          font-size: 1.7rem;
+      }
+
+</style>
